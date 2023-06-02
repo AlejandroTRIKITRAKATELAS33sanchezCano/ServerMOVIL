df5c4bfd7fb818b7928d12e34f71d001786e6491
Alejandro Trikitrakatelas Sanchez Cano <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2023-06-02 17:28:38 -0600

    Puerto configurado

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -1,7 +1,7 @@
 import app from './app.js'
-/*import database from './database.js'*/
-app.listen(5000)
-console.log("Escuchando en el puerto 5000")
+import {PORT} from "./config.js";
+app.listen(PORT)
+console.log(`Escuchando en el puerto ${5000}`)
 
 
 
